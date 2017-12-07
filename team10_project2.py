aa317c191b8d86f04f9fa16fafa38766ce3a49e6
turbophysics <sds158@txstate.edu>
2017-12-06 18:41:51 -0600

    asdf

--- team10_project2.py
+++ team10_project2.py
@@ -1,2 +1,24 @@
-def test_code():
-    print("asd")
+import sys, getopt
+
+ifile = ''
+ofile = ''
+
+
+myopts, args = getopt.getopt(sys.argv[1:], 'i:o:')
+
+for o, a in myopts:
+        if o == '-i':
+                ifile = a
+        elif o == '-o':
+                ofile = a
+
+
+outfile = open(ofile + "_sim.txt", 'w')
+
+
+#read each line of file and put in list
+#with open(ifile, 'r') as infile:
+#    data = infile.read()
+#my_list = data.splitlines()
+
+output_file = open(ofile + "_dis.txt", "w")
